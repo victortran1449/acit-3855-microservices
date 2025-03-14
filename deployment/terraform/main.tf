8057d16ebf8949b76490a390039194132e73bbe5
Victor Tran <victortran1449@gmail.com>
2025-03-13 22:42:12 -0700

    add deployment files

---
+++ deployment/terraform/main.tf
@@ -0,0 +1,122 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+locals {
+  project_name = "acit_3855_microservices"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_vpc" "web" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name    = "project_vpc"
+    Project = local.project_name
+  }
+}
+
+resource "aws_subnet" "web" {
+  vpc_id                  = aws_vpc.web.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Web"
+  }
+}
+
+resource "aws_internet_gateway" "web-gw" {
+  vpc_id = aws_vpc.web.id
+  tags = {
+    Name = "Web"
+  }
+}
+
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.web.id
+  tags = {
+    Name = "web-route"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.web.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.web-gw.id
+}
+
+resource "aws_route_table_association" "web" {
+  subnet_id      = aws_subnet.web.id
+  route_table_id = aws_route_table.web.id
+}
+
+resource "aws_security_group" "web" {
+  name        = "allow_ssh"
+  description = "allow ssh from home and work"
+  vpc_id      = aws_vpc.web.id
+  tags = {
+    Name = "Web"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "web-ssh" {
+  security_group_id = aws_security_group.web.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "web-http" {
+  security_group_id = aws_security_group.web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "web-egress" {
+  security_group_id = aws_security_group.web.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.medium"
+  key_name               = "3855-key"
+  vpc_security_group_ids = [aws_security_group.web.id]
+  subnet_id              = aws_subnet.web.id
+  tags = {
+    Name = "Web"
+  }
+}
+
+output "instance_ip_addr" {
+  description = "The public IP and dns of the web ec2 instance."
+  value = {
+    "public_ip" = aws_instance.web.public_ip
+    "dns_name"  = aws_instance.web.public_dns
+  }
+}
+
