e5dcbb30beea3298c18cab4fec35fabcef91d7db
Victor Tran <114958228+victortran1449@users.noreply.github.com>
2025-03-13 21:32:17 -0700

    add data files

---
+++ data/database/create_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE chat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    stream_id VARCHAR(250) NOT NULL,
+    user_id VARCHAR(250) NOT NULL,
+    message VARCHAR(1000) NOT NULL,
+    reaction_count INTEGER NOT NULL,
+    timestamp VARCHAR(100) NOT NULL,
+    date_created TIMESTAMP NOT NULL DEFAULT NOW(),
+    trace_id VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE donation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    stream_id VARCHAR(250) NOT NULL,
+    user_id VARCHAR(250) NOT NULL,
+    amount FLOAT NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    message VARCHAR(1000) NOT NULL,
+    timestamp VARCHAR(100) NOT NULL,
+    date_created TIMESTAMP NOT NULL DEFAULT NOW(),
+    trace_id VARCHAR(250) NOT NULL
+);
